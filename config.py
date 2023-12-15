d790b0160a3fd8e77d3e01aa666313c9125dd491
Yashdeepsharma2 <139551442+Yashdeepsharma2@users.noreply.github.com>
2023-12-15 07:54:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,20 +9,20 @@ class Config(object):
     
     # db vars
     # keep empty if don't want to add any extra caption
-    CAPTION_TEXT = "Added this caption"  
+    CAPTION_TEXT = "Forwardby : 💓💓YASH💓💓"  
     # BOTTOM or TOP or NIL
     CAPTION_POSITION = "BOTTOM"  
-    ADMIN_USERNAME = "username"  # without "@". 
+    ADMIN_USERNAME = "videodertxt"  # without "@". 
 
     # a list of strings of words to remove from the existing caption
-    WORDS_TO_REMOVE = []  
+    WORDS_TO_REMOVE = [r".*Download.*"]  
     # a list of regex pattern strings to remove from the existing caption. 
     # For eg. r".*Join.*" will remove the entire line having word Join
     REGEX_PATTERNS = []  
 
     # keep empty to allow in all channels. Can add multiple channels separated by a comma.
     # Don't forget -100 before the channel ID
-    ALLOWED_CHANNELS = [-100123456789]
+    ALLOWED_CHANNELS = []
 
     # REMOVE or POSTFIX or NIL. Useful for tamilblasters, tamilmv and other webites
     WEBSITE_PREFIX = "POSTFIX"  
